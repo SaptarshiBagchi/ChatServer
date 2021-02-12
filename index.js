3cebf9b04c5f4c3ce0a2f9bb3b74996b95e1b4d0
Saptarshi <roni.bagchi@gmail.com>
2021-02-13 02:20:37 +0530

    socket message to room not working

--- index.js
+++ index.js
@@ -42,7 +42,9 @@ io.on("connection", socket => {
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit("message", { user: user.name, text: message });
+    socket
+      .to(user.room)
+      .emit("room-message", { user: user.name, text: message });
 
     callback();
   });
